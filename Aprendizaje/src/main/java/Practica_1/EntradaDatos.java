3199d53ccb4b57c94e6c699cac849bc50653f21f
spa51 <santi.15elspa@outlook.com>
2024-05-05 18:08:44 -0500

    carga de datos de entrada

--- Aprendizaje/src/main/java/Practica_1/EntradaDatos.java
+++ Aprendizaje/src/main/java/Practica_1/EntradaDatos.java
@@ -9,9 +9,23 @@ public class EntradaDatos {
     public static void main(String[] args) {
 
         Scanner Entrada =  new Scanner(System.in);
-        String Datos;
-        out.println("mostrar antes");
-        Datos = Entrada.nextLine();
-        out.println("Este es el texto ingresado por teclado: " + Datos);
+        String Nombre;
+        String Profesion;
+        int Edad;
+        double Sueldo;
+
+        out.println("Ingresa tu Nombre: ");
+        Nombre = Entrada.nextLine();
+
+        out.println("Ingresa tu Profesion: ");
+        Profesion = Entrada.nextLine();
+
+        out.println("Ingresa tu Edad");
+        Edad = Entrada.nextInt();
+
+        out.println("Ingresa tu Sueldo");
+        Sueldo = Entrada.nextDouble();
+
+        out.println("Hola " + Nombre+ " Tu profesion es "+ Profesion+" y tu Edad es "+Edad+" y tu sueldo es "+Sueldo);
     }
 }
